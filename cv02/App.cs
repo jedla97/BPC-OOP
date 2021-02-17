a1517ce27ddbbd1007b8c79d77f1645f7c2650de
Jakub Jedlička <jakub.jedla.jedlicka@gmail.com>
2021-02-17 19:58:10 +0100

    add 2nd exercise

---
+++ cv02/App.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cv02
+{
+    class App
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test");
+            Complex c = new Complex(5.1, -8.6);
+            Complex c2 = new Complex(4.1, 7.6);
+            Console.WriteLine(c+c2);
+            Console.WriteLine(c-c2);
+            Console.WriteLine(c*c2);
+            Console.WriteLine(c/c2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ cv02/Complex.cs
@@ -0,0 +1,55 @@
+﻿namespace cv02
+{
+    class Complex
+    {
+        private double real;
+        private double imaginary;
+
+        public Complex(double real = 0, double imaginary = 0)
+        {
+            this.real = real;
+            this.imaginary = imaginary;
+
+        }
+
+        public static Complex operator +(Complex a, Complex b)
+        {
+            return new Complex(a.real + b.real, a.imaginary + b.imaginary);
+        }
+
+        public static Complex operator -(Complex a, Complex b)
+        {
+            return new Complex(a.real - b.real, a.imaginary - b.imaginary);
+        }
+
+        public static Complex operator *(Complex a, Complex b)
+        {
+            return new Complex(a.real*b.real-a.imaginary*b.imaginary, a.real*b.imaginary+a.imaginary*b.real);
+        }
+
+        public static Complex operator /(Complex a, Complex b)
+        { // a + b    c + d
+            return new Complex((a.real*b.real + b.imaginary*a.imaginary)/(b.real*b.real + b.imaginary * b.imaginary), 
+                (b.real*a.imaginary-a.real*b.imaginary)/(b.imaginary*b.imaginary + b.real * b.real)
+                );
+        }
+
+
+
+        public override string ToString()
+        {
+            if (imaginary >= 0)
+            {
+                return string.Format("{0}+{1}j", real, imaginary);
+            }
+            else
+            {
+                return string.Format("{0}-{1}j", real, -imaginary);
+            }
+
+        }
+
+        public double Real { get; set; }
+        public double Imaginary { get; set; }
+    }
+}
