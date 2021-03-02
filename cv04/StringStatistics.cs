4177da39fd0c2e2d4674ddc21887278c4db74dda
Jakub Jedlička <jakub.jedla.jedlicka@gmail.com>
2021-03-02 21:54:03 +0100

    initial comit for 4th exercise

---
+++ cv04/StringStatistics.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cv04
+{
+    public class StringStatistics
+    {
+        public StringStatistics(string text)
+        {
+            this.Text = text;
+        }
+        public string Text { get; set; }
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
+    }
+}
